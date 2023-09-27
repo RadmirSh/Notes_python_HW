4f2ef0cab6283e6df8f86a36d2abc590ede348a4
Radmir Shagiev <veselday@gmail.com>
2023-09-27 16:48:23 +0500

    проверил, добавил ф-цию редактирования заметки

--- notes.py
+++ notes.py
@@ -38,3 +38,16 @@ def delete_note():
             return
     print("Заметка с указанным ID не найдена")
 
+def edit_note():
+    note_id = int(input("Введите ID заметки для редактирования: "))
+    for note in notes:
+        if note['id'] == note_id:
+            new_title = input("Введите новый заголовок заметки: ")
+            new_message = input("Введите новое тело заметки: ")
+            note['title'] = new_title
+            note['message'] = new_message
+            note['timestamp'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            save_notes(notes)
+            print("Заметка успешно отредактирована")
+            return
+    print("Заметка с указанным ID не найдена")
